56f496d0caa0ad7a094500805475e83c68d3d4a8
Aharon Sher <aharonsher@gmail.com>
2024-12-11 12:12:41 +0200

    update 1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Aharon");
+		System.out.println("Hello Aharon1");
 		System.out.println("Hello Ilay second");
 		System.out.println("Hello Shloimi");
 	}
