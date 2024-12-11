a8e2b26c594a9807a599777d838a9a980ae3b24b
Ilay_Nachshon <ilay.na958@gmail.com>
2024-12-11 12:17:09 +0200

    Added 1 from Ilay.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Aharon");
-		System.out.println("Hello Ilay second");
+		System.out.println("Hello Ilay second1");
 		System.out.println("Hello Shloimi");
 	}
 
