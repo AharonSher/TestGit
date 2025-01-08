5e491544031e403951413515b199d58719dd2324
Ilay_Nachshon <ilay.na958@gmail.com>
2025-01-08 10:47:32 +0200

    Added new printing.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.println("Hello Aharon1");
 		System.out.println("Hello Ilay second1");
 		System.out.println("Hello Shloimi");
+		System.out.println("Hello trying branches");
+		
 	}
 
 }
